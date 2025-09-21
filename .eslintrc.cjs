45fae9bd0e83176f6dd9ee03f3806531bfd854e4
Rao-Mubashir <miftech.software.engineer@gmail.com>
2025-09-21 18:19:50 +0500

    pushed for testing

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  root: true,
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
+    "prettier",
+  ],
+  globals: {
+    shopify: "readonly"
+  },
+};
